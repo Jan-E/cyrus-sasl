d5e4e33d2b5bfafaafeb352cb0245170b5c265c7
Jan-E <github@ehrhardt.nl>
2019-01-07 04:50:34 +0100

    OpenSSL 1.0,2 VC14, use libsasl.lib for PHP

--- build-windows/include/config.h
+++ build-windows/include/config.h
@@ -117,7 +117,6 @@ typedef int		    intptr_t;
 /* Windows calls these functions something else
  */
 #define strcasecmp   _stricmp
-#define snprintf    _snprintf
 #define strncasecmp  _strnicmp
 #define strdup       _strdup
 
